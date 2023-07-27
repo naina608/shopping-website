8d28917e793776a0acb8983c7153013fe151b6e2
naina608 <104517150+naina608@users.noreply.github.com>
2023-07-27 19:56:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -138,35 +138,6 @@ app.post('/stripecheckout', async (req, res) => {
 
     res.json(session.url);
 });
-//webhook
-const endpointSecret = process.env.secret_key;
-
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
-
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntentSucceeded = event.data.object;
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
 app.get('/success', async (req, res) => {
     let { order, session_id } = req.query;
     order = decodeURI(order);
