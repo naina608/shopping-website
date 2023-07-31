b143d32673ed654a47509074749cb42f28c4fe9d
608naina <608naina.2020@gmail.com>
2023-07-31 14:55:09 +0530

    new change

--- public/js/cart.js
+++ public/js/cart.js
@@ -4,20 +4,20 @@ function displaycart(){
     document.getElementById("count").innerHTML=cart.length;
     if(cart.length==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-        document.getElementById("total").innerHTML = "$ "+0+".00";
+        document.getElementById("total").innerHTML = "Rs "+0+".00";
     }
     else{
     document.getElementById("cartItem").innerHTML = cart.map((items)=>
         {
                 var {image,price} = items;
                 total=total+price;
-                document.getElementById("total").innerHTML = "$ "+total+".00";
+                document.getElementById("total").innerHTML = "Rs "+total+".00";
                 return( 
                     `<div class='cart-item'>
                     <div class='row-img'>
                         <img class='rowimg' src=${image}>
                     </div>
-                    <h2>$ ${price}.00</h2>`+
+                    <h2>Rs ${price}.00</h2>`+
                     "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
                 );
             }).join('');
